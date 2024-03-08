9fb4b2fc30d39c5737d0c60c2e53849a48d65aee
a-mcmasters <36310514+a-mcmasters@users.noreply.github.com>
2024-03-07 16:08:07 -0800

    Add files via upload

---
+++ Calculator/index.js
@@ -0,0 +1,38 @@
+const display = document.getElementById("display");
+let shouldClearDisplay = false;
+
+function appendToDisplayNum(input) {
+  if (shouldClearDisplay || (display.value === "0" && input !== ".")) {
+    display.value = input === "." ? "0" + input : input;
+    shouldClearDisplay = false;
+  } else {
+    display.value += input;
+  }
+}
+
+function appendToDisplayOp(input) {
+  if (
+    display.value === "ERR" ||
+    display.value === "NaN" ||
+    display.value === "Infinity" ||
+    display.value === "undefined"
+  ) {
+    clearDisplay();
+  }
+  shouldClearDisplay = false;
+  display.value += input;
+}
+
+function clearDisplay() {
+  display.value = "0";
+}
+
+function calculate() {
+  try {
+    const result = math.evaluate(display.value);
+    display.value = result;
+  } catch (error) {
+    display.value = "ERR";
+  }
+  shouldClearDisplay = true;
+}
